32d6ca4e206ce6c1d3817f3d8e22995a26c52480
Nicolas Sursock (work) <nwsursock@Nicolass-MacBook-Pro.local>
2021-08-27 07:13:46 +0300

    added compression

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const app = express();
 const movieRouter = require("./routes/movie.routes");
+const compression = require("compression");
 
+const app = express();
+app.use(compression());
 app.use(express.json());
 app.use(cors());
 app.use(express.static("."), function (req, res, next) {
